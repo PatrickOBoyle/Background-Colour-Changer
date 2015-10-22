8827aa3486aebdc5bb615ea9de281654f0b83ae7
Patrick O'Boyle <oboylepa@tcd.ie>
2015-10-22 09:51:09 +0100

    Simplified implementation
    - Using activeTab more correctly, removed the content.js script and
    moved it’s functionality to the background.js call

--- background.js
+++ background.js
@@ -1,5 +1,26 @@
 // This detects clicks on the Extension Icon (Browser Action Button)
 chrome.browserAction.onClicked.addListener(function (tab) { 
-    // When it is clicked, we want to run our content.js script to change the background
-    chrome.tabs.executeScript(tab.id, {"file": "content.js"});
+    // A default colour to be used if the user has not specified one
+    var DEFAULT_BG_COLOUR = "blue";
+    
+    // Call Chrome's storage to get the user's preference
+    // If they have not specified one, use the default
+    chrome.storage.sync.get({
+        bgColour: DEFAULT_BG_COLOUR
+    
+    }, function(details) {
+        // Assign our background colour to the one chrome has stored
+        backgroundColour = details.bgColour;
+
+        // By using the activeTab permission, we can avoid using a content
+        // script, and instead use the chrome API chrome.tabs to execute our
+        // code here. This is used as a "for all URLs", whereas content scripts
+        // need to have their url's specified.
+        
+        // Change the background of the current window to the
+        // background colour the user's / default value
+        chrome.tabs.executeScript({
+            code: 'document.body.style.backgroundColor="' + backgroundColour + '"'
+        });
+    });
 });
